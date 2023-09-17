73404b50973f894bd4400deeffe6b7651b5275af
fibbbonachos <arish2k3@gmail.com>
2023-09-17 18:45:33 +0530

    update: getVideoTitles function added but videoId and title not in sync wip

--- src/getVideos.js
+++ src/getVideos.js
@@ -1,9 +1,16 @@
+import { getVideoIds } from "./regex.js";
+// import { getVideoTitles } from "./regex.js";
+
 const getVideos = async (query) => {
     try {
         const response = await fetch(
             `https://www.youtube.com/results?search_query=${query}+audio`
         );
         const resultsString = await response.text();
+
+        getVideoIds(resultsString);
+        // getVideoTitles(resultsString);
+
         return resultsString;
     } catch (error) {
         console.log(error);
