b4aa93a6b1cefa9f7d0a3ca3bbac9f1f90781a7d
fibbbonachos <arish2k3@gmail.com>
2023-09-20 11:52:05 +0530

    update: use the same while loop for both functions

--- src/regex.js
+++ src/regex.js
@@ -8,13 +8,14 @@ export function getCleanedData(input) {
     let idIterations = 0;
     let titleIterations = 0;
 
-    while ((idMatch = idRegex.exec(input)) !== null) {
+    while (
+        (idMatch = idRegex.exec(input)) !== null &&
+        (titleMatch = titleRegex.exec(input)) !== null
+    ) {
         console.log(idMatch[0].slice(-12, -1));
-        idIterations++;
-    }
-
-    while ((titleMatch = titleRegex.exec(input)) !== null) {
         console.log(titleMatch[0].slice(27, -3));
+
+        idIterations++;
         titleIterations++;
     }
 
