345a40a405afd9f4ca55ebd3066db569099988c9
fibbbonachos <arish2k3@gmail.com>
2023-09-20 11:44:04 +0530

    update: titles and ids are synced, need to clean up the titles

--- src/regex.js
+++ src/regex.js
@@ -1,3 +1,23 @@
 export function getCleanedData(input) {
-    console.log(input);
+    let idRegex = /\{"videoRenderer":\{"videoId":"[^"]*"/g;
+    let titleRegex = /,"title":\{"runs":\[\{"text":"[^"]*"\}\]/g;
+
+    let idMatch;
+    let titleMatch;
+
+    let idIterations = 0;
+    let titleIterations = 0;
+
+    while ((idMatch = idRegex.exec(input)) !== null) {
+        console.log(idMatch[0].slice(-12, -1));
+        idIterations++;
+    }
+
+    while ((titleMatch = titleRegex.exec(input)) !== null) {
+        console.log(titleMatch[0]);
+        titleIterations++;
+    }
+
+    console.log(idIterations);
+    console.log(titleIterations);
 }
