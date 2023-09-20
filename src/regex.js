1b39982eb7ae658c9959b3aa7124707a418d92b7
fibbbonachos <arish2k3@gmail.com>
2023-09-20 12:12:57 +0530

    update: add videoArtist in json

--- src/regex.js
+++ src/regex.js
@@ -1,32 +1,29 @@
 export function getCleanedData(input) {
     const idRegex = /\{"videoRenderer":\{"videoId":"[^"]*"/g;
     const titleRegex = /,"title":\{"runs":\[\{"text":"[^"]*"\}\]/g;
+    const artistRegex = /,"longBylineText":\{"runs":\[\{"text":"[^"]*"/g;
 
     let idMatch;
     let titleMatch;
-
-    let idIterations = 0;
-    let titleIterations = 0;
+    let artistMatch;
 
     let searchResults = [];
 
     while (
         (idMatch = idRegex.exec(input)) !== null &&
-        (titleMatch = titleRegex.exec(input)) !== null
+        (titleMatch = titleRegex.exec(input)) !== null &&
+        (artistMatch = artistRegex.exec(input)) !== null
     ) {
         console.log(idMatch[0].slice(-12, -1));
         console.log(titleMatch[0].slice(27, -3));
+        console.log(artistMatch[0].slice(36, -1));
 
         searchResults.push({
             videoId: idMatch[0].slice(-12, -1),
             videoTitle: titleMatch[0].slice(27, -3),
+            videoArtist: artistMatch[0].slice(36, -1),
         });
-
-        idIterations++;
-        titleIterations++;
     }
 
-    // console.log(idIterations);
-    // console.log(titleIterations);
     console.log(searchResults);
 }
